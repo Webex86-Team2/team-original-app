1873bec6f4e708ae3440300e8bfe97f6a6632a5c
reinachama <reirei53happy@akane.waseda.jp>
2025-06-01 19:18:21 +0900

    サインアップをつけた

--- app/routes/sign-up.jsx
+++ app/routes/sign-up.jsx
@@ -1,16 +1,30 @@
 import { useState } from "react";
 import "../styles/auth/sign-up.css";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
 export default function SignUp() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  // 送信ボタンを押した時の処理
+  // サインアップ処理を行う関数
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      alert("新規登録しました");
+    } catch (error) {
+      alert("新規登録に失敗しました");
+      console.error(error);
+    }
+  };
   return (
     <div className="sign-up-page">
       <h1>新規登録</h1>
 
       <div className="sign-up-form">
-        <form>
+        <form onSubmit={handleSubmit}>
           {/* メールアドレス */}
           <div className="form-group">
             <label htmlFor="email">メールアドレス</label>
