ef7fe59d774ef507532a0e9b3edf24d7504df8e3
Kotoha7878 <jibundeikiru.77875@gmail.com>
2025-06-09 00:05:11 +0900

    いいねした人のプロフの重なりをなくそうとした。

--- app/routes/favorite.jsx
+++ app/routes/favorite.jsx
@@ -1,19 +1,22 @@
 import UserCard from "../components/UserCard";
 import "../styles/favorite.css";
+import Navbar from "../components/Navbar";
 
 export default function Favorite() {
   return (
-    <div className="favorite">
-      <h1>いいねがありました！</h1>
-      <div className="cards-container">
-        <UserCard />
-
-        <UserCard />
-        <UserCard />
-        <UserCard />
-        <UserCard />
-        <UserCard />
-        <UserCard />
+    <div>
+      <Navbar /> {/*サイドバー or メニューバー */}
+      <div className="favorite">
+        <h1>いいねがありました！</h1>
+        <div className="cards-container">
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+        </div>
       </div>
     </div>
   );
