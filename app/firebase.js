36f65a69a4982d8af39a5501b9abffdce16665fa
Kotoha7878 <jibundeikiru.77875@gmail.com>
2025-06-11 23:45:30 +0900

    プロフィール画面の編集が編集後そのデータが保存し続けられるようにしました。

--- app/firebase.js
+++ app/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCgN70dKVKKwlQGlFTXf5SnF2dXsuQ7adk",
@@ -16,5 +17,6 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { auth, provider, db };
+export { auth, provider, db, storage };
